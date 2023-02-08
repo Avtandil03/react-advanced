ae1d401ee0183495ffb83dfaa8dc3bdc00d1f2ee
Avtandil <avtandilqazxswedc@gmail.com>
2023-02-08 22:47:26 +0300

    add dropdown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,49 @@
 import React, { useState, useEffect }  from 'react';
-import { useSearchUsersQuery } from '../store/github/github.api';
+import { useSearchUsersQuery } from '../store/github/github.api'
+import { useDebounce } from '../hooks/debounce'
 
 const HomePage = () => {
   const [search, setSearch] = useState('')
-  const {isLoading, isError, data} = useSearchUsersQuery('Avtandil')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3
+  })
 
   useEffect(() => {
-    console.log(search)
-  }, [search])
+    setDropdown(debounced.length > 3 && data?.length! > 1)
+  }, [debounced, data])
 
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen' >
-      {isError && <p className='text-center text-red-600' >Something went wrong...</p>}
-      
-      <div className='relative w-[560px]'>
-        <input
-          type='text'
-          className='border py-2 px-4 w-full h-[42px] mb-2'
-          placeholder='Search for GitHub username...'
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        <div className="absolute top-[42px] w-full left-0 rigth-0 max-h-[200px] shadow-md bg-white">
-          Lorem ipsum dolor sit.
-        </div>
+    {isError && <p className='text-center text-red-600' >Something went wrong...</p>}
+    
+    <div className='relative w-[560px]'>
+      <input
+        type='text'
+        className='border py-2 px-4 w-full h-[42px] mb-2'
+        placeholder='Search for GitHub username...'
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      {dropdown &&
+       <ul 
+        className="list-none absolute top-[42px] w-full left-0 rigth-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+        {isLoading && <p className='text-center'>Loading...</p>}
+        {data?.map(user => (
+          <li
+            key={user.id}
+            className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+          >
+            {user.login}
+          </li>
+        ))}
+       </ul>
+      }
 
-      </div>
     </div>
-  );
-};
+  </div>
+  )
+}
 
 export default HomePage;
